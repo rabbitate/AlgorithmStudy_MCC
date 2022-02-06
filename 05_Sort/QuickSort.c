ecf808d3a1784038a985a3a220f7f5acf53eee06
Minpyo Kim <56537513+rabbitate@users.noreply.github.com>
2022-02-06 17:57:39 +0900

    QuickSort 주석 추가

--- 05_Sort/QuickSort.c
+++ 05_Sort/QuickSort.c
@@ -15,14 +15,17 @@ void Swap(int* a, int* b) {
 }
 
 void QuickSort(int* array, int start, int end) {
-    if(start>=end) return;
+    if(start>=end) return; // 등호를 통해 리스트 크기가 1인 경우를 걸러준다
     
     int pivot = start;
     
     int l = start+1;
     int r = end;
     
+    // l과 r이 엇갈릴 때까지
     while(l<=r) {
+        // 2개의 while문 중 pivot과의 비교에서의 등호가 1개 이상 필요
+        // -> 등호가 2개 다 없다면, pivot 과 l,r 번째 인덱스의 값이 다 같을때 l,r 값 변화가 없다
         while(l <= end && array[l] <= array[pivot]) {
             l++;
         }
